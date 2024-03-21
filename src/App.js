70fa428266a28f244d3ddfe6cd4d7c9cf42cf1cf
Meet Gauswami <meetgauswami01@gmail.com>
2024-03-21 15:23:09 +0530

    add just in head

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function App() {
 <div className="container my-3">
 <Routes>
          <Route exact path="/about" element={<About mode={mode} />}></Route>
-           <Route exact path="/"  element={ <TextForm showAlert={showAlert} heading = "Try TextUtils - Word Counter, Character Counter, Remove Extra Spaces" mode={mode}/>  }  ></Route>
+           <Route exact path="/"  element={ <TextForm showAlert={showAlert} heading = "Just Try TextUtils - Word Counter, Character Counter, Remove Extra Spaces" mode={mode}/>  }  ></Route>
 </Routes> 
 </div>
 
